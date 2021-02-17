86cb7e782198ba24c1f28ec92cacc79b42941e5a
ShubhamBhardwaj <shubham.11901502@lpu.in>
2021-02-17 15:54:31 +0530

    user by id query

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,8 +1,13 @@
 const Query = {
   users(parent, args, { db }, info) {
-    if (!args.query) {
+    if (!nameQuery) {
       return db.users;
+    } else if (args.nameQuery) {
     }
+  },
+  userById(parent, args, { db }, info) {
+    const reqUser = db.users.find((x) => x.id === args.idQuery);
+    return reqUser;
   },
   repos(parent, args, { db }, info) {
     if (!args.query) {
