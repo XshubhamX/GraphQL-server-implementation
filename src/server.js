e8217428748132e517b0da2112a06f474927143d
ShubhamBhardwaj <shubham.11901502@lpu.in>
2021-02-18 15:04:55 +0530

    new structure

--- src/server.js
+++ src/server.js
@@ -2,12 +2,14 @@ import { GraphQLServer } from "graphql-yoga";
 import db from "./db";
 import Query from "./resolvers/Query";
 import User from "./resolvers/User";
+import Repo from "./resolvers/Repo";
 
 const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
   resolvers: {
     Query,
     User,
+    Repo,
   },
   context: {
     db,
